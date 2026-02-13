3d3aa64b823da59e5d1f56883ceff6e31ddedbbc
Galatear <171226854+LadyGalatear@users.noreply.github.com>
2026-02-12 22:13:14 -0800

    Add solutions to rest

--- q3.js
+++ q3.js
@@ -1,6 +1,13 @@
 // 3. Remove duplicates from an array (using a loop). FYI: array.includes(element) checks if an element is in an array
 function removeDuplicates(array) {
+    let unique = [];
+    for (let i = 0; i < array.length; i++) {
+        if (!unique.includes(array[i])) {
+            unique.push(array[i]);
+        }
+    }
 
+    return unique;
 }
 
 // Test for remove duplicates
@@ -8,3 +15,5 @@ function removeDuplicates(array) {
 let testArray3 = [1, 2, 3, 4, 5, 1, 2, 3, 4, 5]; // [1, 2, 3, 4, 5]
 
 // TODO: display the array with duplicates removed in the console
+
+console.log(removeDuplicates(testArray3));
--- q4.js
+++ q4.js
@@ -3,7 +3,13 @@ A palindrome is a word, number, or other sequence of characters that reads the s
 */
 
 function checkPalindrome(textInput) {
-    
+    for (let i = 0; i < textInput.length / 2; i++) {
+        if (textInput[i] != textInput[textInput.length - 1 - i]) {
+            return false;
+        }
+    }
+
+    return true;
 }
 
 // Test for palindrome
@@ -12,3 +18,6 @@ let testString = "racecar"; // true
 let testString2 = "hello"; // false
 
 // TODO: display the result of the test strings in the console
+
+console.log(checkPalindrome(testString));
+console.log(checkPalindrome(testString2));
